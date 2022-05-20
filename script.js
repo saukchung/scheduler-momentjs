ffbd70e67c01390087a7452ac6b298ad50ae5fb3
Sa Uk Chung <chungsauk95@gmail.com>
2022-05-19 20:31:56 -0400

    Stuck, didn't change much. Working only with the first time block at the moment.

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
+//DEPENDENCIES
 var dateDisp = moment().format("MMMM Do, dddd");
+var currentHour = moment().format("H")
+
+var container = $(".container");
+var btn = $(".btn");
+
+var block9 = $("#9");
+var input9 = $("input[name='input9']").val();
+
+
+//STATES
+
+//FUNCTIONS
+// Change color when the hour is in
+function colorTime(currentHour) {
+
+};
+
+
+//USER INTERACTIONS
+
+//INITIALIZATION
+//  displays today's date
 $("#currentDay").text(dateDisp);
